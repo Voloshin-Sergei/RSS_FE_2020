16f220b25721f0003c289a9d348a662be891c5cd
Voloshin_Sergey <4voloshin@gmail.com>
2020-10-24 16:26:14 +0300

    fix: burger menu from mane page

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,7 +1,8 @@
     const burger = document.querySelector('.burger-menu__button');
     const mobileMenu = document.querySelector('.mobile-menu');
     const overlay = document.querySelector('.burger-menu_overlay');
-    const links = document.querySelector('.list');
+    const links = document.querySelector('.list__link');
+    const body = document.querySelector('.body');
     let burgerShow = false;
 
 function burgerMenu() {
@@ -9,10 +10,12 @@ function burgerMenu() {
         burger.classList.remove('burger-menu__button_active');
         mobileMenu.classList.remove('mobile-menu_active');
         overlay.classList.remove('burger-menu_overlay-active');
+        body.classList.remove('overflow-hidden');
     } else {
         burger.classList.add('burger-menu__button_active');
         mobileMenu.classList.add('mobile-menu_active');
         overlay.classList.add('burger-menu_overlay-active');
+        body.classList.add('overflow-hidden');
     }
     burgerShow = !burgerShow;
 };
