0b4d942219604967b2c42928dffe569ebdad704b
Voloshin_Sergey <4voloshin@gmail.com>
2020-12-01 16:30:46 +0300

    feat: add menu

--- english-for-kids/js/index.js
+++ english-for-kids/js/index.js
@@ -1,9 +1,19 @@
-const checkbox = document.getElementById('checkbox');
-const label = document.querySelector('label');
+const checkbox = document.querySelector('.checkbox');
+const label = document.querySelector('.label');
+const menuButton = document.querySelector('.menu-button');
+const navigation = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
 
 const startGame = () => {
   document.body.classList.toggle('game');
   label.classList.toggle('game');
+  menu.classList.toggle('game');
+};
+
+const showMenu = () => {
+  menuButton.classList.toggle('is-active');
+  navigation.classList.toggle('nav-visible');
 };
 
 checkbox.addEventListener('click', startGame);
+menuButton.addEventListener('click', showMenu);
